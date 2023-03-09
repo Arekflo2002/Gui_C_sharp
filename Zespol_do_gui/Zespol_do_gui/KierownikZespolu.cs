32c917e11d2e544de6af2e2b19ba703eea001684
Arekflo2002 <120436902+Arekflo2002@users.noreply.github.com>
2023-03-09 20:09:53 +0100

    Add files via upload

---
+++ Zespol_do_gui/Zespol_do_gui/KierownikZespolu.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.Serialization;
+
+namespace Zespol_do_gui
+{
+    [DataContract]
+    public class KierownikZespolu : Osoba, ICloneable
+    {
+        #region Fields
+        [DataMember]
+        int doswiadczenieKierownicze; // liczba przepracowanych lat
+        [DataMember]
+        long telefonKontaktowy;
+
+        public int doswiadczenie { get { return doswiadczenieKierownicze; } set { doswiadczenieKierownicze = value; } }
+        #endregion Fields
+
+        #region Constructors
+        public KierownikZespolu() { }
+        public KierownikZespolu(string imie, string nazwisko, EnumPlec plec) 
+            : base(imie, nazwisko, plec)
+        { }
+        public KierownikZespolu(string imie, string nazwisko, string dataUrodzenia,
+            string pesel, EnumPlec plec) 
+            : base(imie, nazwisko, dataUrodzenia, pesel, plec)
+        { }
+        public KierownikZespolu(string imie, string nazwisko, string dataUrodzenia,
+            string pesel, EnumPlec plec, int doswiadczenie, long telefon)
+            : this(imie, nazwisko, dataUrodzenia, pesel, plec)
+        {
+            doswiadczenieKierownicze = doswiadczenie;
+            telefonKontaktowy = telefon;
+        }
+        #endregion Constructors
+
+        #region Methods
+        public override string ToString()
+        {
+            return $"{base.ToString()}, {doswiadczenieKierownicze}, [{telefonKontaktowy:000-000-000}]";
+        }
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+        #endregion Methods
+    }
+}
